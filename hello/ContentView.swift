3653e6abd355d41f56746790a633aaff3cab94a2
CJ Wheelan <cjwheelan@CJs-MacBook-Pro-2.local>
2021-08-23 11:15:40 -0400

    First commit

---
+++ hello/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  hello
+//
+//  Created by CJ Wheelan on 5/7/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, this app was made by CJ!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
